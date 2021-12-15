3d0ab1ff1c015628ab1f7ffa124b00c87226ed12
zhujian <zhujian@ewell.cc>
2021-12-15 16:11:18 +0800

    [jdk1.8.0_261-源码初始提交]

---
+++ src/com/cdxz/test/Test.java
@@ -0,0 +1,16 @@
+package com.cdxz.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "我");
+        map.put("2", "爱");
+        map.put("3", "中");
+        map.put("4", "中国");
+    }
+
+}
